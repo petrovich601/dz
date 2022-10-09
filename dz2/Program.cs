5cc8fe0acaa6f000ed8abe6e8b080c9e58e6611d
jury <kulibin.76@mail.ru>
2022-10-09 19:22:25 +0500

    zdh10

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -2,3 +2,9 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите число N:");
+int nam = Convert.ToInt32(Console.ReadLine());
+int nam3 = nam3 = nam / 100;
+int nam2 = (nam - (nam3 *100)) / 10;
+Console.WriteLine(nam2);
